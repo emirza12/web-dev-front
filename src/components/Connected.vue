b7afcea601d113167ccdb24e2910d3de518a96fd
emirza12 <eva.mirza@free.fr>
2025-03-30 17:48:16 +0200

    logout

--- src/components/Connected.vue
+++ src/components/Connected.vue
@@ -1,6 +1,17 @@
+<script lang="ts" setup>
+import router from '../handlers/index.js';
+
+function logout() {
+  localStorage.removeItem('authToken');
+  router.push('/signin');
+}
+</script>
+
 <template>
-    <div >
-        <h1>Welcome!</h1>
-        <p>You are now successfully connected to your account.</p>
-    </div>
+  <div>
+    <h1>Welcome!</h1>
+    <p>You are now successfully connected to your account.</p>
+    <button @click="logout">Logout</button>
+  </div>
 </template>
+
